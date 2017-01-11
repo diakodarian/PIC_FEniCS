958f8b2520a6e7f7648eef45e862755ba03d6146
diakodarian <diako.darian@gmail.com>
2017-01-11 23:46:01 +0100

    mesh

--- FieldSolver.py
+++ FieldSolver.py
@@ -43,40 +43,8 @@ def periodic_solver(f, mesh, V, V_e):
     solver.solve(uh.vector(), b)
 
     # Compute gradient
-    method1 = False
-    if method1:
-        v = TestFunction(V_e)
-        w = TrialFunction(V_e)
-
-        a = inner(w, v)*dx
-        L = inner(grad(uh), v)*dx
-        A, b = assemble_system(a, L)
-        grad_u = Function(V_e)
-
-        # Create Krylov solver
-        solver = PETScKrylovSolver("cg")
-        solver.set_operator(A)
-
-        # Create vector that spans the null space and normalize
-        null_vec = Vector(grad_u.vector())
-        V.dofmap().set(null_vec, 1.0)
-        null_vec *= 1.0/null_vec.norm("l2")
-
-        # Create null space basis object and attach to PETSc matrix
-        null_space = VectorSpaceBasis([null_vec])
-        as_backend_type(A).set_nullspace(null_space)
-
-        # Orthogonalize RHS vector b with respect to the null space (this
-        # gurantees a solution exists)
-        null_space.orthogonalize(b);
-
-        # Solve
-        solver.solve(-1*grad_u.vector(), b)
-    else:
-        grad_u = Function(V_e)
-        grad_u = project(-1*grad(uh), V_e)
-
-    #grad_u_x, grad_u_y = grad_u.split(deepcopy=True)  # extract component
+    grad_u = Function(V_e)
+    grad_u = project(-1*grad(uh), V_e)
 
     return uh, grad_u
 
@@ -87,19 +55,67 @@ def dirichlet_solver(f, V, V_g, bc):
     v = TestFunction(V)
     a = dot(grad(u), grad(v))*dx
     L = f*v*dx
-    A, b = assemble_system(a, L, bc)
+
     # Compute solution
     u = Function(V)
-    solver = KrylovSolver(A, "cg", "petsc_amg")
-    solver.solve(u.vector(), b)
+    solve(a == L, u, bc)
 
     # Compute the gradient
     grad_u = Function(V_g)
     grad_u = project(-1*grad(u), V_g)
+
     return u, grad_u
+
+def test_periodic_solver():
+    divs = [[3, 3], [5, 3], [3, 5], [20,20]]
+    L = [[0., 0., 1., 1.], [-1., -1, 0, 2., 1., 1.]]
+    tol = 1E-10
+
+    for i in range(len(divs)):
+        print("run: ", i)
+        divisions = divs[i]
+        print('divisions: ', divisions)
+        mesh = UnitHyperCube(divisions)
+        #mesh = HyperCube(L[0], divisions)
+        V, V_g = periodic_bcs(mesh, L[0])
+
+        class Source(Expression):
+            def eval(self, values, x):
+                values[0] = sin(2.0*DOLFIN_PI*x[0]) + sin(2.0*DOLFIN_PI*x[1])
+
+        class Exact(Expression):
+            def eval(self, values, x):
+                values[0] = (sin(2.0*DOLFIN_PI*x[0]) + sin(2.0*DOLFIN_PI*x[1]))/\
+                            (4.0*DOLFIN_PI*DOLFIN_PI)
+
+        f = Source(degree=2)
+        phi, E = periodic_solver(f, mesh, V, V_g)
+
+        phi_e = Exact(degree=2)
+
+        # vertex_values_phi_e = phi_e.compute_vertex_values(mesh)
+        # vertex_values_phi = phi.compute_vertex_values(mesh)
+        #
+        # error_max = np.max(vertex_values_phi_e - \
+        #                     vertex_values_phi)
+        #
+        # msg = 'error_max = %g' %error_max
+        # assert error_max < tol , msg
+
+        e_x = Expression('-2*x[0]', degree=1)
+        e_y = Expression('-4*x[1]', degree=1)
+        # The gradient:
+        E_pro = project(E, VectorFunctionSpace(mesh, 'CG', 1))
+        flux_u_x, flux_u_y = E_pro.split(deepcopy=True)
+
+        plot(phi, interactive=True)
+        plot(E, interactive=True)
+
 
 if __name__ == '__main__':
     import time
+    from mesh_types import *
+    from boundary_conditions import *
 
     def run_periodic_solver():
         L = [-1., 1.,-1., 1.]
@@ -149,22 +165,22 @@ if __name__ == '__main__':
         #plot(ff,  interactive=True)
         plot(phi, interactive=True)
         plot(E, interactive=True)
-        grad_u_x, grad_u_y = E.split(deepcopy=True)  # extract components
-
-        u_array = phi.vector().array()
-        grad_u_x_array = grad_u_x.vector().array()
-        grad_u_y_array = grad_u_y.vector().array()
-        coor = mesh.coordinates()
-        for i in range(len(u_array)):
-            x, y = coor[i]
-            print 'Node (%.3f,%.3f): u = %.4f (%9.2e), '\
-                  'grad(u)_x = %.4f  (%9.2e), grad(u)_y = %.4f  (%9.2e)' % \
-                  (x, y, u_array[i], (-1.*sin(2.0*DOLFIN_PI*x))/(4.0*DOLFIN_PI*DOLFIN_PI) - u_array[i],
-                   grad_u_x_array[i], cos(2.0*DOLFIN_PI*x)/(2.0*DOLFIN_PI) - grad_u_x_array[i],
-                   grad_u_y_array[i], 0 - grad_u_y_array[i])
-                #   (x, y, u_array[i], (sin(4.0*DOLFIN_PI*x) + sin(4.0*DOLFIN_PI*y))/(16.0*DOLFIN_PI*DOLFIN_PI) - u_array[i],
-                #    grad_u_x_array[i], cos(4.0*DOLFIN_PI*x)/(4.0*DOLFIN_PI) - grad_u_x_array[i],
-                #    grad_u_y_array[i], cos(4.0*DOLFIN_PI*y)/(4.0*DOLFIN_PI) - grad_u_y_array[i])
+        # grad_u_x, grad_u_y = E.split(deepcopy=True)  # extract components
+        #
+        # u_array = phi.vector().array()
+        # grad_u_x_array = grad_u_x.vector().array()
+        # grad_u_y_array = grad_u_y.vector().array()
+        # coor = mesh.coordinates()
+        # for i in range(len(u_array)):
+        #     x, y = coor[i]
+        #     print 'Node (%.3f,%.3f): u = %.4f (%9.2e), '\
+        #           'grad(u)_x = %.4f  (%9.2e), grad(u)_y = %.4f  (%9.2e)' % \
+        #           (x, y, u_array[i], (-1.*sin(2.0*DOLFIN_PI*x))/(4.0*DOLFIN_PI*DOLFIN_PI) - u_array[i],
+        #            grad_u_x_array[i], cos(2.0*DOLFIN_PI*x)/(2.0*DOLFIN_PI) - grad_u_x_array[i],
+        #            grad_u_y_array[i], 0 - grad_u_y_array[i])
+        #           (x, y, u_array[i], (sin(4.0*DOLFIN_PI*x) + sin(4.0*DOLFIN_PI*y))/(16.0*DOLFIN_PI*DOLFIN_PI) - u_array[i],
+        #            grad_u_x_array[i], cos(4.0*DOLFIN_PI*x)/(4.0*DOLFIN_PI) - grad_u_x_array[i],
+        #            grad_u_y_array[i], cos(4.0*DOLFIN_PI*y)/(4.0*DOLFIN_PI) - grad_u_y_array[i])
 
     def run_Dirichlet_solver():
         # Source term
@@ -180,7 +196,7 @@ if __name__ == '__main__':
                 values[0] = x[0]*exp(-pow(pow(2*(x[0]-0.5),2)+pow(2*(x[1]-0.5),2),2))
 
         # Create mesh and finite element
-        mesh = UnitSquareMesh(200, 200)
+        mesh = UnitSquareMesh(20, 20)
         V = FunctionSpace(mesh, "CG", 1)
         V_g = VectorFunctionSpace(mesh, 'CG', 1)
 
@@ -193,12 +209,13 @@ if __name__ == '__main__':
 
         f = Source1(degree=1)
         phi, E = dirichlet_solver(f, V, V_g, bc)
-
-        File("data/phi.pvd") << phi
-        File("data/partitions.pvd") << CellFunction("size_t", mesh, rank)
+        plot(phi, interactive=True)
+        plot(E, interactive=True)
+        #File("data/phi.pvd") << phi
+        #sFile("data/partitions.pvd") << CellFunction("size_t", mesh, rank)
 
     # tic = time.time()
-    # run_Dirichlet_solver()
+    #run_Dirichlet_solver()
     # toc = time.time()
     # if rank == 0:
     #     seconds = toc-tic
@@ -206,4 +223,5 @@ if __name__ == '__main__':
     #     m, s = divmod(seconds, 60)
     #     h, m = divmod(m, 60)
     #     print "Total run time: %d:%02d:%02d" % (h, m, s)
-    run_periodic_solver()
+
+    test_periodic_solver()
--- mesh_types.py
+++ mesh_types.py
@@ -14,23 +14,23 @@ def HyperCube(coordinates, divisions):
                             *divisions)
     return mesh
 
+def test_Unit_mesh():
+    divs = [[10,10], [10,10,10]]
+    for i in range(len(divs)):
+        divisions = divs[i]
+        mesh = UnitHyperCube(divisions)
+        plot(mesh, interactive = True)
+
+def test_mesh():
+    divs = [[10,10], [10,10,10]]
+    L = [[-1., -1, 2., 1.], [-1., -1, 0, 2., 1., 1.]]
+    for i,j in zip(L, divs):
+        L = i
+        divisions = j
+        mesh = HyperCube(L, divisions)
+        plot(mesh, interactive = True)
+
 if __name__=='__main__':
-
-    def test_Unit_mesh():
-        divs = [[10,10], [10,10,10]]
-        for i in range(len(divs)):
-            divisions = divs[i]
-            mesh = UnitHyperCube(divisions)
-            plot(mesh, interactive = True)
-
-    def test_mesh():
-        divs = [[10,10], [10,10,10]]
-        L = [[-1., -1, 2., 1.], [-1., -1, 0, 2., 1., 1.]]
-        for i,j in zip(L, divs):
-            L = i
-            divisions = j
-            mesh = HyperCube(L, divisions)
-            plot(mesh, interactive = True)
 
     test_Unit_mesh()
     test_mesh()
